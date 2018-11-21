5b8df7469b655838b94aec3ca18d983fde0d4f44
Atle Nordland <atle.nordland@nav.no>
2018-11-21 12:26:42 +0100

    add tests

--- src/main/kotlin/no/nav/dagpenger/journalføring/manuell/GsakHttpClient.kt
+++ src/main/kotlin/no/nav/dagpenger/journalføring/manuell/GsakHttpClient.kt
@@ -6,13 +6,17 @@ import com.github.kittinunf.result.Result
 import com.google.gson.Gson
 import no.nav.dagpenger.oidc.OidcClient
 
-class GsakHttpClient(private val gsakUrl: String, private val oidcClient: OidcClient) {
+class GsakHttpClient(private val gsakUrl: String, private val oidcClient: OidcClient) : GsakClient {
 
-    fun createManuellJournalføringsoppgave(request: ManuellJournalføringsoppgaveRequest): ManuellJournalføringsoppgaveResponse {
+    override fun createManuellJournalføringsoppgave(
+        request: ManuellJournalføringsoppgaveRequest,
+        correlationId: String
+    ): ManuellJournalføringsoppgaveResponse {
         val url = "${gsakUrl}v1/oppgaver"
         val json = Gson().toJson(request).toString()
         val (_, response, result) = with(url.httpPost().body(json)) {
             header("Authorization" to oidcClient.oidcToken().access_token.toBearerToken())
+            header("X-Correlation-ID" to correlationId)
             responseObject<ManuellJournalføringsoppgaveResponse>()
         }
 
@@ -52,34 +56,34 @@ data class ManuellJournalføringsoppgaveRequest(
 )
 
 data class ManuellJournalføringsoppgaveResponse(
-    val id: Int?,
-    val tildeltEnhetsnr: String?,
-    val endretAvEnhetsnr: String?,
-    val opprettetAvEnhetsnr: String?,
-    val journalpostId: String?,
-    val journalpostkilde: String?,
-    val behandlesAvApplikasjon: String?,
-    val saksreferanse: String?,
-    val bnr: String?,
-    val samhandlernr: String?,
-    val aktoerId: String?,
-    val orgnr: String?,
-    val tilordnetRessurs: String?,
-    val beskrivelse: String?,
-    val temagruppe: String?,
-    val tema: String?,
-    val behandlingstema: String?,
-    val oppgavetype: String?,
-    val behandlingstype: String?,
+    val id: Int? = null,
+    val tildeltEnhetsnr: String? = null,
+    val endretAvEnhetsnr: String? = null,
+    val opprettetAvEnhetsnr: String? = null,
+    val journalpostId: String? = null,
+    val journalpostkilde: String? = null,
+    val behandlesAvApplikasjon: String? = null,
+    val saksreferanse: String? = null,
+    val bnr: String? = null,
+    val samhandlernr: String? = null,
+    val aktoerId: String? = null,
+    val orgnr: String? = null,
+    val tilordnetRessurs: String? = null,
+    val beskrivelse: String? = null,
+    val temagruppe: String? = null,
+    val tema: String? = null,
+    val behandlingstema: String? = null,
+    val oppgavetype: String? = null,
+    val behandlingstype: String? = null,
     val versjon: Int,
-    val mappeId: Int?,
-    val fristFerdigstillelse: String,
+    val mappeId: Int? = null,
+    val fristFerdigstillelse: String? = null,
     val aktivDato: String,
-    val opprettetTidspunkt: String?,
-    val opprettetAv: String?,
-    val endretAv: String?,
-    val ferdigstiltTidspunkt: String?,
-    val endretTidspunkt: String?,
+    val opprettetTidspunkt: String? = null,
+    val opprettetAv: String? = null,
+    val endretAv: String? = null,
+    val ferdigstiltTidspunkt: String? = null,
+    val endretTidspunkt: String? = null,
     val prioritet: Prioritet,
     val status: String,
     val metadata: Map<String, String>? = null
